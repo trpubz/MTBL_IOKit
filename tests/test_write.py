97f7054d5bcc6c57a72a5b7e141dfd74abb71f3e
Taylor "TP" Pubins <tpubz@icloud.com>
2024-02-26 14:30:25 -0700

    Tests all lines of write.py

--- tests/test_write.py
+++ tests/test_write.py
@@ -1,5 +1,7 @@
 import os
-from unittest.mock import patch  # For mocking functions like os.path
+import tempfile
+import json
+import csv
 import pytest
 import pandas as pd
 
@@ -15,29 +17,58 @@ class TestWrite:
     def setup_and_teardown(self):
         self.df = pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]})
         yield
-        os.remove(self.full_path)
 
-    @patch("mtbl_playerkit")
-    def test_write_out_player_serialized(self):
-        player = Player(name="Corbin Carrol", team="ARI")
-        write.write_out(player)
+    @pytest.fixture
+    def temp_dir(self):
+        with tempfile.TemporaryDirectory() as dir_path:
+            yield dir_path
+
+    def test_write_out_player_serialized(self, temp_dir):
+        plyr1 = Player(name="Corbin Carrol", team="ARI")
+        plyr2 = Player(name="Zac Gallen", team="ARI")
+
+        # Use the temporary directory
+        write.write_out([plyr1, plyr2], temp_dir, "player_data", ".json")
+
+        # Verify file presence
+        file_path = os.path.join(temp_dir, "player_data.json")
+        assert os.path.exists(file_path)
+
+        # Verify content (Pydantic serialization)
+        with open(file_path, 'r') as f:
+            data = json.load(f)
+            assert len(data) == 2
+            assert data[0]['name'] == "Corbin Carrol"
+
+    def test_write_out_non_pydantic_model(self, temp_dir):
+        csv = """name, team
+                    corbin carrol, ARI"""
+
+        write.write_out(csv, temp_dir, "player_data", ".csv")
+        # Verify file presence
+        file_path = os.path.join(temp_dir, "player_data.csv")
+        assert os.path.exists(file_path)
 
     def test_export_dataframe_to_csv(self):
         write.export_dataframe(self.df, "test", ".csv", "./temp")
         self.full_path = "./temp/test.csv"
+
         assert os.path.exists(self.full_path)
 
     def test_export_dataframe_to_json(self):
         write.export_dataframe(self.df, "test", ".json", "./temp")
         self.full_path = "./temp/test.json"
+
         assert os.path.exists(self.full_path)
 
     def test_export_dataframe_to_json_without_schema(self):
         write.export_dataframe(self.df, "test", ".json", "./temp", schema=False)
         self.full_path = "./temp/test.json"
+
         assert os.path.exists(self.full_path)
 
     def test_export_dataframe_without_proper_file_type(self):
         write.export_dataframe(self.df, "test", "json", "./temp")
         self.full_path = "./temp/test.json"
+
         assert os.path.exists(self.full_path)
--- write/write.py
+++ write/write.py
@@ -8,10 +8,10 @@ import pandas as pd
 from mtbl_playerkit import Player
 
 
-def write_out(data, directory: str, file_name: str, ext: str):
+def write_out(data: any, directory: str, file_name: str, ext: str):
     """
     Write files + create directories if not yet present
-    :param data: data to write
+    :param data: any type of data to write
     :param directory: str directory to write to
     :param file_name: str name of file to write
     :param ext: str extension of file to write
@@ -19,18 +19,15 @@ def write_out(data, directory: str, file_name: str, ext: str):
     """
     # w+: Opens a file for both writing and reading. Overwrites the existing file if the file exists
     # If the file does not exist, creates a new file for reading and writing.
-    if isinstance(data.first, Player):
-        serialized = [x.__dict__ for x in data]  # serialize Player objects
-        with open(os.path.join(directory, (file_name + ext)), mode='w+', encoding='utf-8') as f:
+    with open(os.path.join(directory, (file_name + ext)), mode='w+', encoding='utf-8') as f:
+        if isinstance(data[0], Player):
+            # Pydantic's 'dict()' method for each object
+            serialized = [x.model_dump() for x in data]
             json.dump(serialized, f, indent=2)
-            print(f'{file_name}{ext} successfully saved to {directory}')
-            f.close()
-    else:
-        with open(os.path.join(directory, (file_name + ext)), mode='w+', encoding='utf-8') as f:
-            # print(content, file=f)
+        else:
             f.write(data)
-            # print(f'{fileName}{ext} successfully saved to {getDir(dir)}')
-            f.close()
+
+        f.close()
 
 
 def export_dataframe(
